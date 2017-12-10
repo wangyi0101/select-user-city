2b3994a0f13da41ec9c0eef499a4c8db3a60ec2d
yi <wangyi0101@gmail.com>
2017-12-09 19:42:35 -0800

    Add style

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,9 +1,10 @@
 import React  from 'react';
+import './Dropdown.css';
 
 function Dropdown (props) {
     let {list, onClick} = props;
     return list.map((item, index) => {
-        return <p key={index}><a onClick={onClick}>{item}</a></p>
+        return <p key={index} onClick={onClick}>{item}</p>
     })
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import User from './components/user/User';
-import Controller from './components/controller/controller'
+import Controller from './components/controller/Controller'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Controller />, document.getElementById('root'));
